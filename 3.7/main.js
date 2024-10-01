f8e7fe024dbf5149523301ff39eb3696d0177ced
Shadi Abdelsalam <floydanthebeast@gmail.com>
2024-10-01 22:45:16 +0300

    feat: Finish task 3.7

---
+++ 3.7/main.js
@@ -0,0 +1,39 @@
+const body = document.querySelector('body');
+const cursor = document.querySelector('#cursor');
+
+function moveCursor(e) {
+  cursor.style.left = `${e.clientX - cursor.clientWidth / 2}px`;
+  cursor.style.top = `${e.clientY - cursor.clientHeight / 2}px`;
+}
+
+function spawnRipple(e) {
+  const ripple = document.createElement('div');
+
+  ripple.classList.add('ripple');
+  ripple.style.display = 'none';
+
+  body.appendChild(ripple);
+
+  setTimeout(() => {
+    ripple.style.display = 'unset';
+    ripple.style.left = `${e.clientX - ripple.offsetWidth / 2}px`;
+    ripple.style.top = `${e.clientY - ripple.offsetHeight / 2}px`;
+    setTimeout(() => body.removeChild(ripple), 500);
+  }, 0);
+}
+
+window.addEventListener('mousemove', moveCursor);
+window.addEventListener('click', spawnRipple);
+document.addEventListener('mouseleave', function (event) {
+  if (
+    event.clientY <= 0 ||
+    event.clientX <= 0 ||
+    event.clientX >= window.innerWidth ||
+    event.clientY >= window.innerHeight
+  ) {
+    cursor.style.display = 'none';
+  }
+});
+document.addEventListener('mouseenter', function () {
+  cursor.style.display = 'unset';
+});
